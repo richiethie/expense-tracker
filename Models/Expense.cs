9fc71c37e5646747bf176ce498738fa0aff8d0f9
Richie Thiesfeldt <rthiesfeldt@roydan.com>
2024-10-16 15:24:06 -0500

    initial working app

---
+++ Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        //[Required]
+        public string? Description { get; set; }
+    }
+}
